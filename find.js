a52206729745b37f4d2f0efe17dfefcbad50f7e4
anme <shekhar>
2017-02-13 12:43:06 +0530

    commented code...

--- find.js
+++ find.js
@@ -1,23 +1,24 @@
 var mongoose = require('mongoose');
-var conn = mongoose.createConnection('mongodb://127.0.0.1/user');
-var detail_schema = mongoose.Schema({
+var conn = mongoose.createConnection('mongodb://127.0.0.1/user'); // connection to the database
+var detail_schema = mongoose.Schema({   
 name: String,
 },{
     strict: true,
     collection: 'detail'
-});
-var detail_object = conn.model('detail', detail_schema);
+}); // collection detail schema....
+var detail_object = conn.model('detail', detail_schema); // creating object of collection schema...
 var details = detail_object.find({
-    name: /sau/i
+    name: /saurabh0/i                        // here i is flag which set if data is find...
 }, function (err, result) {
+    console.log(result.length);
     if (err) {
         console.log(err);
         process.exit();
     }
     else {
-        for (var i = 0; i < result.length; i++) {
-            var row = result[i];
-            console.log(row);
+        for (var j = 0; j < result.length; j++) {
+            var row = result[j];
+            console.log(result);
             process.exit ();
         }
 
