9f31be17885c76d54ff1ed117359c61ca6531c48
anme <shekhar>
2017-02-15 17:31:57 +0530

    final update

--- database/db.js
+++ database/db.js
@@ -1,21 +1,21 @@
 // the middleware function
-module.exports = function () {
+module.exports = function() {
     var mongoose = require('mongoose');
     mongoose.connect('mongodb://127.0.0.1/details');
- 
+
     var conn = mongoose.connection;
     var users_schema = mongoose.Schema({
-    	firstname:String,
-    	email:String,
-    	password:String
+        firstname: String,
+        email: String,
+        password: String
     }, {
         strict: true,
         collection: 'Users'
     });
     var userobject = conn.model('user', users_schema); // user object
-    return function (req, res, next) {
-        req.object= userobject;
+    return function(req, res, next) {
+        req.object = userobject;
         next();
     }
- 
+
 };
--- user.js
+++ user.js
@@ -4,27 +4,29 @@ var bodyParser = require('body-parser'); // required body-parser module
 const crypto = require('crypto'); // module for using md5 function
 var db = require('./database/db.js');
 app.use(db());
-app.use(bodyParser.urlencoded({				// getting the data from url
+app.use(bodyParser.urlencoded({ // getting the data from url
     extended: true
 }));
 app.post("/", function(req, res) { //getting post data from form...
     var name = req.body.name;
     var email = req.body.email;
     var pass = req.body.password;
-    console.log(name + email + pass);
-    var password = pass;
-    var hash = crypto.createHash("md5", password).digest('hex');
-    var detail = new req.object({
-        "firstname": name,
-        "email": email,
-        "password": hash,
-    });
-    detail.save(function(err, records) // data save in database
-        {
-            if (err) {
-                throw error;
-            } else {
-                res.send("data saved");
-            }
+    var hash = crypto.createHash("md5", pass).digest('hex');
+    if (name == "" || email == "" || pass == "") {
+        res.send("some fields are missing");
+    } else {
+        var detail = new req.object({
+            "firstname": name,
+            "email": email,
+            "password": hash,
         });
+        detail.save(function(err, records) // data save in database
+            {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.send("data saved");
+                }
+            });
+    }
 }).listen(8080); //listning at 8080 port
