e6158c42305f127371bf62d1e40f1d5000edb9f2
anme <shekhar>
2017-02-17 09:56:20 +0530

    remove data

--- routes/get.js
+++ routes/get.js
@@ -2,29 +2,44 @@ var express = require('express'); // require express module
 var app = express(); // creatig insatnce of express function
 var router = express.Router();
 router.all('/users/:id', function(req, res) {
-    var userid = req.params.id
-    var object = req.object;
-    object.find({
+	var userid = req.params.id
+	var object = req.object;
+	object.find({
         '_id': userid // here i is flag which set if data is find...
     }, function(err, result) {
-        if (err) {
-            res.json({
-                status: 0,
-                message: 'user data is not found'
-            });
-        } else {
-            if (result.length == 0) {
-                res.json({
-                    status: 0,
-                    message: 'user data is not found'
-                });
-            } else {
-                res.json({
-                    status: 1,
-                    message: result
-                });
-            }
-        }
+    	if (err) {
+    		res.json({
+    			status: 0,
+    			message: 'user data is not found'
+    		});
+    	} else {
+    		if (result.length == 0) {
+    			res.json({
+    				status: 0,
+    				message: 'user data is not found'
+    			});
+    		} else {
+    			res.json({
+    				status: 1,
+    				message: result
+    			});
+    		}
+    	}
     });
 });
+
+//        data deletion from mongodb collection................
+
+router.all('/user/:email', function(req, res) {
+	var email = req.params.email;
+	var object = req.object;
+	object.remove({email:email}, function(err, result) {
+		if (err) {
+			res.json(err);
+		}else
+		res.json("data not found");
+	});
+
+});
+
 module.exports = router;
